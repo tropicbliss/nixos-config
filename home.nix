3150caff09770a911ad8c46100b7885c4f778af1
tropicbliss <tropicbliss@protonmail.com>
2025-01-24 12:09:12 +0800

    add svelte ext

--- home.nix
+++ home.nix
@@ -74,9 +74,10 @@
   programs.vscode = {
     enable = true;
     package = pkgs.vscodium;
-    extensions = with pkgs; [
-      vscode-extensions.bbenoist.nix
-      vscode-extensions.mkhl.direnv
+    extensions = with pkgs.vscode-extensions; [
+      bbenoist.nix
+      mkhl.direnv
+      svelte.svelte-vscode
     ];
   };
   programs.direnv = {
